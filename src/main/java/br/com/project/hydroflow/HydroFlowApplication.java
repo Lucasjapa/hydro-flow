97b7795ad1cec6f1ff4c3bb09d02aabf5cddd6f7
Lucas Silva <lucas.cavalcante@pitang.com>
2026-03-13 19:51:06 -0300

    Initial commit

---
+++ src/main/java/br/com/project/hydroflow/HydroFlowApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.hydroflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydroFlowApplication.class, args);
+	}
+
+}
